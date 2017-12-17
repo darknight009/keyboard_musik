9b6342b3bc107e5af76fb2ce280f7871bfb456d5
Bhaavan Goel <cbhaavan@gmail.com>
2017-12-18 03:17:45 +0530

    notes are played via pygame

--- Keyboard_musik/main.py
+++ Keyboard_musik/main.py
@@ -1,25 +1,38 @@
 from __future__ import print_function
 import time
-from playsound import playsound
+import sys
+import pygame.midi
 from inputs import get_key
 
 def main():
+	pygame.midi.init()
+	player = pygame.midi.Output(0)
+	player.set_instrument(0)
 	print("started")
 	sd=-1
 	prev=-1
+	t0=0
 	while 1:
 		events = get_key()
 		if events:
 			for event in events:
-				print(event.ev_type, event.code, event.state)
 				sd=event.state
-				print(sd)
 				break
 			print(prev, sd)
-			if prev!=sd:
-				playsound('notes/sound ('+str(sd)+').wav')
-				prev=sd
+		if prev==sd:
+			if t0==0:
+				t0=time.time()
+		else:
+			t1=time.time()
+			player.note_on(prev+36, 127)
+			if t0==0:
+				t0=t1-0.1
+			print(t1-t0)
+			time.sleep(t1-t0)
+			player.note_off(prev+36, 127)
+			prev=sd
+			t0=0
 
 
 if __name__ == "__main__":
-	main()
+	main() 
